6cf71dd7424df6bd5000daad1ad41138a205fbf6
Allan Mitanda <alan@globalwesttechnologies.org>
2025-12-28 13:33:30 +0300

    Authentication for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User  # Using Django's built-in User
+# from django.contrib.auth.models import User  # Using Django's built-in User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES=(
+        ('client', 'Client')
+        ('hairdresser', 'Hairdresser')
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='client')
+    
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    role = serializers.ChoiceField(choices=User.ROLE_CHOICES, default='client')
+
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'role']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            role=validated_data['role']
         )
+        if user.role == 'hairdresser':
+            user.is_staff = True  # Example: make hairdressers staff users
+            user.save()
+            from hairdressers.models import HairdresserProfile
+            HairdresserProfile.objects.create(user=user, phone='', location='')
         return user
 
 class LoginSerializer(serializers.Serializer):
